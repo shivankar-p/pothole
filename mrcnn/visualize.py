973207a64d722069dbade12a76e4ec40a6325b99
shivankar-p <shivankarpilligundla@gmail.com>
2023-01-21 12:00:17 +0530

    added sample videos

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -75,7 +75,7 @@ def apply_mask(image, mask, color, alpha=0.5):
     for c in range(3):
         image[:, :, c] = np.where(mask == 1,
                                   image[:, :, c] *
-                                  (1 - alpha) + alpha * color[c] * 255,
+                                  (1 - alpha) + alpha * color[c],
                                   image[:, :, c])
     return image
 
